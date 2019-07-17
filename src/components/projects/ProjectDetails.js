6b35c18cf1a0798407c2e894f02b529f6701396b
Vadim <chakapega@gmail.com>
2019-07-17 04:10:07 +0300

    Completed projects components

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function ProjectDetails(props) {
+  const id = props.match.params.id;
+
+  return (
+    <div className='container section project-details'>
+      <div className='card z-depth-0'>
+        <div className='card-content'>
+          <span className='card-title'>Project title - {id}</span>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam sint unde illum facilis doloribus debitis vel nam magni harum. Quae alias in minus quidem praesentium!</p>
+        </div>
+        <div className='card-action grey lighten-4 grey-text'>
+          <div>Posted by chakapega</div>
+          <div>July 17th, 3am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
