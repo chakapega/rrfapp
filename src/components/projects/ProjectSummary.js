9d071082d7c521abc21290de602427465cf5eaf4
Vadim <chakapega@gmail.com>
2019-07-18 00:51:15 +0300

    introduction redux

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function ProjectSummary() {
+export default function ProjectSummary({ project }) {
   return (
     <div className='card z-depth-0 project-summary'>
     <div className='card-content grey-text text-darken-3'>
-      <span className='card-title'>Project title</span>
+      <span className='card-title'>{project.title}</span>
       <p>Posted by Vadim</p>
       <p className='grey-text'>July 17th, 3am</p>
     </div>
